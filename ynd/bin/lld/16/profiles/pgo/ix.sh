0c7ac0af6dcaa3c2cacd3aae35d5f320b0e941ff
mikhnenko <mikhnenko@yandex-team.com>
2024-07-17 15:26:38 +0300

    Update pgo profile once again dce8019abee019c676a65e36163be1ea0707b76f

--- ynd/bin/lld/16/profiles/pgo/ix.sh
+++ ynd/bin/lld/16/profiles/pgo/ix.sh
@@ -1,7 +1,7 @@
 {% extends '//clang/16/profiles/t.sh' %}
 
 {% block fname %}pgo-merged.prof{% endblock %}
-{% block sandbox_id %}6671018356{% endblock %}
+{% block sandbox_id %}6697745148{% endblock %}
 
 {% block env %}
 export PGO_EXTERNAL_PROFILE=${out}/share/{{self.fname().strip()}}
