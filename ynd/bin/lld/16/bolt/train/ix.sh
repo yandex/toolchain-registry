2d7cc18fe2e9c475a2fa5f05f58876a8d2586145
mikhnenko <mikhnenko@yandex-team.com>
2024-07-19 21:54:22 +0300

    Merge perf profiles profiles a044cfb5ee59be4db7a9dfbebdb451bc2a3ef96b

--- ynd/bin/lld/16/bolt/train/ix.sh
+++ ynd/bin/lld/16/bolt/train/ix.sh
@@ -14,10 +14,10 @@ bin/perf
 {% block setup_tools %}
 cat << EOF > perf_lld
 #!/usr/bin/env sh
-perf record --output ${tmp}/profiles/lld_perf --timestamp-filename --event cycles:u --branch-filter any,u -- ${LLD_EXT_PROFILE_PATH} "\${@}"
+perf record --output ${tmp}/perf_data/lld_perf --timestamp-filename --event cycles:u --branch-filter any,u -- ${LLD_EXT_PROFILE_PATH} "\${@}"
 EOF
 chmod +x perf_lld
-mkdir ${tmp}/profiles
+mkdir ${tmp}/perf_data
 {{super()}}
 {% endblock %}
 
@@ -30,7 +30,14 @@ export LDFLAGS="--ld-path=${tmp}/bin/ut/perf_lld ${LDFLAGS}"
 {{super()}}
 echo ${LLD_EXT_PROFILE_PATH} > ${out}/linker_abs_path
 sed -i -e 's|ld.lld|lld|g' ${out}/linker_abs_path
-cd ${tmp}
-llvm-bolt $(find . -wholename "./profiles/lld_perf*" | while read data; do echo -p ${data}; done) -o ${out}/bolt.prof -aggregate-only $(cat ${out}/linker_abs_path)
+LLD_ABS_PATH=$(cat ${out}/linker_abs_path)
+mkdir ${tmp}/bolt_data
+cd ${tmp}/perf_data
+find . -name "lld_perf*" | while read data; do
+    perf2bolt -p ${data} -o ../bolt_data/${data}.fdata ${LLD_ABS_PATH}
+done
+cd ${tmp}/bolt_data
+merge-fdata *.fdata > combined.fdata
+llvm-bolt -data=${tmp}/bolt_data/combined.data -o ${out}/bolt.prof -aggregate-only ${LLD_ABS_PATH}
 rm -rf ${out}/bin ${out}/lib ${out}/share ${out}/include
 {% endblock %}
