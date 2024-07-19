e419690c3e9e98a1d967c63b16f8cde2bf889b81
mikhnenko <mikhnenko@yandex-team.com>
2024-07-19 17:00:40 +0300

    Collect perf for bolt 27a63722f9e9259d67c4560bf190a87fc4673053

--- ynd/bin/lld/16/bolt/train/ix.sh
+++ ynd/bin/lld/16/bolt/train/ix.sh
@@ -1,16 +1,36 @@
 {% extends '//clang/16/ix.sh' %}
 
-{% block bld_tool %}
-{{super()}}
-bin/lld/16/clang
-bin/lld/16/pgo/ext-profile
+{% block c_compiler %}
+bld/compiler(clang_ver=16)
 {% endblock %}
 
-{% block configure %}
-export LDFLAGS="--ld-path=${LLD_EXT_PROFILE_PATH} ${LDFLAGS}"
+{% block bld_tool %}
 {{super()}}
+bin/lld/16/pgo/ext-profile
+bin/bolt
+bin/perf
+{% endblock %}
+
+{% block setup_tools %}
+cat << EOF > perf_lld
+#!/usr/bin/env sh
+perf record --output ${tmp}/profiles/lld_perf --timestamp-filename --event cycles:u --branch-filter any,u -- ${LLD_EXT_PROFILE_PATH} "\${@}"
+EOF
+chmod +x perf_lld
+mkdir ${tmp}/profiles
+{{super()}}
+{% endblock %}
+
+{% block setup_target_flags %}
+{{super()}}
+export LDFLAGS="--ld-path=${tmp}/bin/ut/perf_lld ${LDFLAGS}"
 {% endblock %}
 
 {% block postinstall %}
-echo ${LLD_EXT_PROFILE_PATH} > "${out}/linker_abs_path"
+{{super()}}
+echo ${LLD_EXT_PROFILE_PATH} > ${out}/linker_abs_path
+sed -i -e 's|ld.lld|lld|g' ${out}/linker_abs_path
+cd ${tmp}
+llvm-bolt $(find . -wholename "./profiles/lld_perf*" | while read data; do echo -p ${data}; done) -o ${out}/bolt.prof -aggregate-only $(cat ${out}/linker_abs_path)
+rm -rf ${out}/bin ${out}/lib ${out}/share ${out}/include
 {% endblock %}
