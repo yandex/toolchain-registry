1ff816c896960586365e3638606fd20e8be19763
nechda <nechda@yandex-team.com>
2024-10-16 16:38:50 +0300

    Download prebuild-win32 binaries from python.org commit_hash:858fea1f20ba8e3627bcf22c5f480e9b5140012f

--- ynd/bin/frozen-python3/ix.sh
+++ ynd/bin/frozen-python3/ix.sh
@@ -1,5 +1,9 @@
 {% extends '//die/hub.sh' %}
 
 {% block run_deps %}
+{% if mingw32 %}
+ynd/bin/frozen-python3/win32
+{% else %}
 ynd/bin/frozen-python3/unwrap(python_ver=12)
+{% endif %}
 {% endblock %}
---
+++ ynd/bin/frozen-python3/win32/ix.sh
@@ -0,0 +1,32 @@
+{% extends '//die/std/ix.sh' %}
+
+{% block bld_tool %}
+bin/wget
+bin/unzip
+{% endblock %}
+
+{% block use_network %}true{% endblock %}
+
+{% block py_version %}3.12.6{% endblock %}
+{% block src_url %}https://www.python.org/ftp/python/{{self.py_version().strip()}}/python-{{self.py_version().strip()}}-embed-win32.zip{% endblock %}
+
+{% block step_unpack %}
+mkdir src
+wget {{self.src_url().strip()}} -O src.zip
+{% endblock %}
+
+{% block predict_outputs %}
+[{"path": "python3-mingw", "sum": "b6eecbdfd865e4a3ae8bed93b9f2cd95"}]
+{% endblock %}
+
+{% block step_build %}
+set -xue
+unzip src.zip -d bin/ && rm src.zip
+cd bin && unzip python312.zip && rm python312.zip
+{% endblock %}
+
+{% block install %}
+mv ${tmp}/* ${out}
+{% endblock %}
+
+{% block strip_bin %}:{% endblock %}
