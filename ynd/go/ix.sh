2fa7e6bdb589e773942794aba468bb4ce8c17dc2
mikhnenko <mikhnenko@yandex-team.com>
2025-04-02 15:25:06 +0300

    Use block fetch instead of direct wget commit_hash:f23b48ff62ee77bf285cc0d870db96c821ad2ce6

--- ynd/go/ix.sh
+++ ynd/go/ix.sh
@@ -1,48 +1,47 @@
 {% extends '//die/std/ix.sh' %}
 
-{% block bld_tool %}
-bin/wget
-{% endblock %}
-
-{% block use_network %}true{% endblock %}
-
-{% block step_unpack %}
-mkdir src
-{% endblock %}
-
-{% block predict_outputs %}
-[{"path": "go", "sum": ""}]
-{% endblock %}
-
 {% block go_version %}
 1.23.7
 {% endblock %}
 
-{% block go_src %}
+{% block archive_hash %}
 {% if linux and x86_64 %}
-go{{self.go_version().strip()}}.linux-amd64.tar.gz
+    4741525e69841f2e22f9992af25df0c1112b07501f61f741c12c6389fcb119f3
 {% elif linux and aarch64 %}
-go{{self.go_version().strip()}}.linux-arm64.tar.gz
+    597acbd0505250d4d98c4c83adf201562a8c812cbcd7b341689a07087a87a541
 {% elif darwin and x86_64 %}
-go{{self.go_version().strip()}}.darwin-amd64.tar.gz
+    3a3d6745286297cd011d2ab071998a85fe82714bf178dc3cd6ecd3d043a59270
 {% elif darwin and arm64 %}
-go{{self.go_version().strip()}}.darwin-arm64.tar.gz
+    a08a77374a4a8ab25568cddd9dad5ba7bb6d21e04c650dc2af3def6c9115ebba
 {% elif mingw32 %}
-go{{self.go_version().strip()}}.windows-amd64.zip
+    eba0477381037868738b47b0198d120a535eb9a8a17b2babb9ab0d5e912a2171
 {% endif %}
 {% endblock %}
 
+{% block archive_name %}
+{% if linux and x86_64 %}
+    linux-amd64.tar.gz
+{% elif linux and aarch64 %}
+    linux-arm64.tar.gz
+{% elif darwin and x86_64 %}
+    darwin-amd64.tar.gz
+{% elif darwin and arm64 %}
+    darwin-arm64.tar.gz
+{% elif mingw32 %}
+    windows-amd64.zip
+{% endif %}
+{% endblock %}
+
+{% block fetch %}
+https://go.dev/dl/go{{self.go_version().strip()}}.{{self.archive_name().strip()}}
+sha:{{self.archive_hash().strip()}}
+{% endblock %}
+
 {% block step_build %}
-set -xue
-wget https://go.dev/dl/{{self.go_src().strip()}}
-tar xf {{self.go_src().strip()}} || unzip {{self.go_src().strip()}}
-rm {{self.go_src().strip()}} 
-cd go
 sed -i 's/GOTOOLCHAIN=auto/GOTOOLCHAIN=local/g' go.env
 rm -r "test/fixedbugs/issue27836.dir"
 {% endblock %}
 
 {% block install %}
-mv ${tmp}/go/* ${out}
+mv ${tmp}/src/* ${out}
 {% endblock %}
-
