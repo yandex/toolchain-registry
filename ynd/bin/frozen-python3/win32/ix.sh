5687bd2c8b4b523b25fa21f0e27dc7a03208f336
nechda <nechda@yandex-team.com>
2025-06-30 20:01:08 +0300

    Fix frozen python win32 script commit_hash:b1b1f470aa576ca43463883ff5c6e29f71e555fa

--- ynd/bin/frozen-python3/win32/ix.sh
+++ ynd/bin/frozen-python3/win32/ix.sh
@@ -5,29 +5,27 @@ bin/wget
 bin/unzip
 {% endblock %}
 
-{% block use_network %}true{% endblock %}
-
 {% block py_version %}3.12.6{% endblock %}
 {% block src_url %}https://www.python.org/ftp/python/{{self.py_version().strip()}}/python-{{self.py_version().strip()}}-embed-win32.zip{% endblock %}
 
-{% block step_unpack %}
-mkdir src
-wget {{self.src_url().strip()}} -O src.zip
+{% block fetch %}
+{{self.src_url().strip()}}
+sha:8c1b9cd9f63001a58f986f8570a5ab79276e8c1a048f7bfe3efe631a878e189c
 {% endblock %}
 
-{% block predict_outputs %}
-[{"path": "python3-mingw", "sum": "b6eecbdfd865e4a3ae8bed93b9f2cd95"}]
-{% endblock %}
 
 {% block step_build %}
 set -xue
+cp ${src}/python-{{self.py_version().strip()}}-embed-win32.zip src.zip
+mkdir bin
 unzip src.zip -d bin/ && rm src.zip
+
 cd bin && unzip python312.zip && rm python312.zip
 mv python.exe python3
 {% endblock %}
 
 {% block install %}
-mv ${tmp}/* ${out}
+mv ${tmp}/src/* ${out}
 
 mkdir -p ${out}/fix
 cat << EOF > ${out}/fix/remove_tmp.sh
