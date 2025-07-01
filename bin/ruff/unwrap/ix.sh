196dd76fee0b44e7e8812e6e4a578569fa8e648d
pg <pg@yandex-team.com>
2025-07-01 09:09:14 +0300

    Untitled commit commit_hash:6b99c5b7ae23b860d265448c7413fab03262ca84

--- bin/ruff/unwrap/ix.sh
+++ bin/ruff/unwrap/ix.sh
@@ -1,47 +1,21 @@
-{% extends '//die/rust/cargo.sh' %}
-
-{% block pkg_name %}
-ruff
-{% endblock %}
+{% extends '//ext/ix/pkgs/bin/ruff/unwrap/ix.sh' %}
 
 {% block version %}
 0.12.0
-{% endblock %}
-
-{% block cargo_url %}
-https://github.com/astral-sh/ruff/archive/refs/tags/{{self.version().strip()}}.tar.gz
 {% endblock %}
 
 {% block cargo_sha %}
 56ccbf0c506c604439e06f9f7c12a2031dc7d831dee33b312bb182dd85e4cd2e
 {% endblock %}
 
-{% block cargo_bins %}
-ruff
+{% block cargoc_ver %}
+bld/cargo/86
+{% endblock %}
+
+{% block rustc_ver %}
+86
 {% endblock %}
 
 {% block cargo_ver %}
 v4
-{% endblock %}
-
-{% block bld_libs %}
-lib/c
-lib/zstd
-bin/ruff/{{target.os}}
-{{super()}}
-{% endblock %}
-
-{% block host_libs %}
-lib/c
-lib/zstd
-{{super()}}
-{% endblock %}
-
-{% block cargo_flags %}
-{{super()}}
---config 'target.x86_64-pc-windows-gnullvm.linker = "clang"'
-{% endblock %}
-
-{% block cargo_tool %}
-bld/cargo/86
 {% endblock %}
