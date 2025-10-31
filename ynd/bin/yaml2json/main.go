2d006f47a81fd9556d6fa30e7dae1c01b96f3bbc
nechda <nechda@yandex-team.com>
2025-10-31 23:13:50 +0300

    Allow reading from stdin yaml2json commit_hash:baf7e747fe1e800e015b658195111972c2db0efd

--- ynd/bin/yaml2json/main.go
+++ ynd/bin/yaml2json/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
@@ -11,18 +12,28 @@ import (
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: yaml2json <input.yaml>")
+		fmt.Println("If <input.yaml> is `-` then content reads from stdio")
 		os.Exit(1)
 	}
 
 	inputFile := os.Args[1]
-
-	yamlData, err := ioutil.ReadFile(inputFile)
-	if err != nil {
-		log.Fatalf("Error reading YAML file: %v", err)
+	yamlData := []byte{}
+	if inputFile == "-" {
+		data, err := io.ReadAll(os.Stdin)
+		if err != nil {
+			log.Fatalf("Error reading YAML from stdin: %v", err)
+		}
+		yamlData = data
+	} else {
+		data, err := ioutil.ReadFile(inputFile)
+		if err != nil {
+			log.Fatalf("Error reading YAML file: %v", err)
+		}
+		yamlData = data
 	}
 
 	var data interface{}
-	err = Unmarshal(yamlData, &data)
+	err := Unmarshal(yamlData, &data)
 	if err != nil {
 		log.Fatalf("Error parsing YAML: %v", err)
 	}
@@ -34,4 +45,3 @@ func main() {
 
 	fmt.Println(string(jsonData))
 }
-
