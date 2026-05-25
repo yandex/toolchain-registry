bc52c2c2685e78a97d9ce59b47f1500c39bd1bd8
mikhnenko <mikhnenko@yandex-team.com>
2026-05-25 18:50:51 +0300

    Add clang22 commit_hash:7982d845d794e5e95be59d4a85f2e02ccc8cd18e

---
+++ ynd/clang/22/template.sh
@@ -0,0 +1,92 @@
+{% extends '//bin/clang/20/ix.sh' %}
+
+{% block clang_major_version %}
+22
+{% endblock %}
+
+{% block fetch %}
+{% include '//ynd/lib/llvm/22/ver.sh' %}
+{% endblock %}
+
+{% block cmake_flags %}
+{{super()}}
+{% if linux and x86_64 %}
+LLVM_ENABLE_PROJECTS="clang;clang-tools-extra;lld;polly"
+{% endif %}
+LLVM_ENABLE_LIBXML2="FORCE_ON"
+{% endblock %}
+
+{% block llvm_targets %}
+{{super()}}
+clang-format
+clang-refactor
+{% if linux and x86_64 %}
+clang-tidy
+{% endif %}
+llvm-cov
+llvm-profdata
+llvm-rc
+sancov
+{% endblock %}
+
+{% block bld_libs %}
+ynd/lib/xml2
+{{super()}}
+{% endblock %}
+
+
+{% block clang_patches %}
+clang-format-patches.patch
+asan_static.patch
+D21113-case-insesitive-include-paths.patch
+clang-statistics-used-bytes.patch
+{% endblock %}
+
+{% block common_patches %}
+{% endblock %}
+
+{% block llvm_patches %}
+vfs-case-insensitive.patch
+dont-remove-dbg-info.patch
+fix-objcopy-elf.patch
+{% endblock %}
+
+{% block patch %}
+{{super()}}
+{% for p in self.common_patches().strip().split() %}
+(base64 -d | patch -p1) << EOF
+{{ix.load_file('//clang/22/patches/clang/' + p) | b64e}}
+EOF
+{% endfor %}
+cd llvm
+{% for p in self.llvm_patches().strip().split() %}
+(base64 -d | patch -p1) << EOF
+{{ix.load_file('//clang/22/patches/llvm/' + p) | b64e}}
+EOF
+{% endfor %}
+cd ..
+cd clang
+echo $(pwd)
+{% for p in self.clang_patches().strip().split() %}
+(base64 -d | patch -p1) << EOF
+{{ix.load_file('//clang/22/patches/clang/' + p) | b64e}}
+EOF
+{% endfor %}
+cd ..
+{% endblock %}
+
+{% block postinstall %}
+cd ${out}/bin
+{% if linux and x86_64 %}
+ln -s llvm-cov llvm-gcov
+{% endif %}
+{% if mingw32 %}
+ln -s llvm-ar.exe llvm-lib.exe
+{% else %}
+ln -s llvm-ar llvm-lib
+{% endif %}
+{% endblock %}
+
+{% block clang_fix_includes %}
+:
+{% endblock %}
---
+++ ynd/lib/llvm/22/ver.sh
@@ -0,0 +1,2 @@
+https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-22.1.6.tar.gz
+ba534c6835a5b9c2162c806e269799fe41fca952a3c25baff1afcff23841ec2b
