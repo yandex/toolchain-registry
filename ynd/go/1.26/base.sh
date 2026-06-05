9ba1bc85826b1f1a5014928a3f11ffbbba91f732
mikhnenko <mikhnenko@yandex-team.com>
2026-06-05 14:53:33 +0300

    Support  old coverage in go_tool 1.26 commit_hash:127ec0da888a8e7c4f12a3626b3a3dfe51fb9890

--- ynd/go/1.26/base.sh
+++ ynd/go/1.26/base.sh
@@ -37,6 +37,12 @@ curl 'https://go.dev/dl/?mode=json&include=all' | jq -r '.[] | select(.version==
 {% endif %}
 {% endblock %}
 
+{% block step_patch %}
+(base64 -d | patch -p1 -r /dev/null --no-backup-if-mismatch) << EOF
+{{ix.load_file('//go/1.26/old_coverage.diff') | b64e}}
+EOF
+{% endblock %}
+
 {% block fetch %}
 https://go.dev/dl/go{{self.go_version().strip()}}.{{self.archive_name().strip()}}
 sha:{{self.archive_hash().strip()}}
